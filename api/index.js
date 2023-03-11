d99dbc3d66eddd5d554e046097136eda2d87e061
JJ-practice <fixymefixyou@gmail.com>
2023-03-11 23:24:47 +0800

    corsv1

--- api/index.js
+++ api/index.js
@@ -63,14 +63,20 @@ app.listen(process.env.PORT || 5000, () => {
   console.log('Backend is Running')
 })
 
-app.use(function (req, res, next) {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://serene-jalebi-075250.netlify.app'
-  )
+///CORS handling middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', [
+    'https://serene-jalebi-075250.netlify.app/',
+    'https://frontend.netlify.app',
+  ])
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
+
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
   next()
 })
