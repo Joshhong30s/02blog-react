fbbd8c8eed2e954afb001d0ba37015ed87448f96
JJ-practice <fixymefixyou@gmail.com>
2023-03-11 12:58:20 +0800

    SSLV10

--- api/index.js
+++ api/index.js
@@ -19,8 +19,8 @@ app.use('/images', express.static(path.join(__dirname, '/images')))
 
 mongoose
   .connect(process.env.MongoURL, {
-    sslKey: fs.readFileSync('./certificate.pem'),
-    sslCert: fs.readFileSync('./certificate.pem'),
+    sslKey: fs.readFileSync('./'),
+    sslCert: fs.readFileSync('./'),
     serverApi: ServerApiVersion.v1,
   })
   .then(console.log('connected to MongoDB'))
